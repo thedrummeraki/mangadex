6860bf4a823c6ed924b348cc411c2b29af0e1a4b
Akinyele Cafe-Febrissy <me@akinyele.ca>
2021-09-15 22:07:27 -0400

    Add scanlation group API

--- lib/mangadex/author.rb
+++ lib/mangadex/author.rb
@@ -4,6 +4,23 @@ module Mangadex
   class Author < MangadexObject
     has_attributes :name, :image_url, :biography, :version, :created_at, :updated_at
 
+    class << self
+      def list(**args)
+      end
+
+      def create(**args)
+      end
+
+      def get(id, **args)
+      end
+
+      def update(id, **args)
+      end
+
+      def delete(id)
+      end
+    end
+
     def self.inspect_attributes
       [:name]
     end
--- lib/mangadex/scanlation_group.rb
+++ lib/mangadex/scanlation_group.rb
@@ -14,9 +14,83 @@ module Mangadex
       :version,
       :created_at,
       :updated_at
-  end
 
-  def self.inspect_attributes
-    self.attributes - [:version, :created_at, :updated_at]
+    class << self
+      def list(**args)
+        Mangadex::Internal::Request.get(
+          '/group',
+          Mangadex::Internal::Definition.validate(args, {
+            limit: { accepts: Integer },
+            offset: { accepts: Integer },
+            ids: { accepts: [String] },
+            name: { accepts: String },
+            includes: { accepts: [String] },
+          }),
+        )
+      end
+
+      def create(**args)
+        Mangadex::Internal::Request.post(
+          '/group',
+          payload: Mangadex::Internal::Definition.validate(args, {
+            name: { accepts: String, required: true },
+            website: { accepts: String },
+            irc_server: { accepts: String },
+            irc_channel: { accepts: String },
+            discord: { accepts: String },
+            contact_email: { accepts: String },
+            description: { accepts: String },
+          }),
+        )
+      end
+
+      def view(id)
+        Mangadex::Internal::Request.get(
+          '/group/%{id}' % {id: id},
+          Mangadex::Internal::Definition.validate(args, {
+            includes: { accepts: [String] },
+          }),
+        )
+      end
+
+      def update(id, **args)
+        Mangadex::Internal::Request.put(
+          '/group/%{id}' % {id: id},
+          payload: Mangadex::Internal::Definition.validate(args, {
+            name: { accepts: String },
+            website: { accepts: String },
+            irc_server: { accepts: String },
+            irc_channel: { accepts: String },
+            discord: { accepts: String },
+            contact_email: { accepts: String },
+            description: { accepts: String },
+            locked: { accepts: [true, false] },
+            version: { accepts: Integer, required: true },
+          }),
+        )
+      end
+
+      def delete(id)
+        Mangadex::Internal::Request.delete(
+          '/group/%{id}' % {id: id},
+        )
+      end
+
+      def follow(id)
+        Mangadex::Internal::Request.post(
+          '/group/%{id}/follow' % {id: id},
+        )
+      end
+
+      def unfollow(id)
+        Mangadex::Internal::Request.delete(
+          '/group/%{id}/follow' % {id: id},
+        )
+      end
+    end
+
+    def self.inspect_attributes
+      self.attributes - [:version, :created_at, :updated_at]
+    end
   end
 end
