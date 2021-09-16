61ea44f46057c455db36285ff57a2b930a896e3f
Akinyele Cafe-Febrissy <me@akinyele.ca>
2021-09-15 22:58:52 -0400

    Add version mismatch warning

--- lib/mangadex/api/version.rb
+++ lib/mangadex/api/version.rb
@@ -3,11 +3,18 @@ require "psych"
 module Mangadex
   module Api
     VERSION = -> do
-      Psych.load(
+      version = Psych.load(
         RestClient.get(
           'https://api.mangadex.org/api.yaml',
         ).body,
       ).dig('info', 'version')
+
+      if version != Mangadex::VERSION
+        warn(
+          "[Warning] This gem is compatible with #{Mangadex::VERSION} but it looks like Mangadex is at #{version}",
+          "[Warning] Check out #{Mangadex::Internal::Request::BASE_URI} for more information.",
+        )
+      end
     end.call
   end
 end
