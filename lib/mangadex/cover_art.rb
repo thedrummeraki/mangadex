2f07540b1b70377c70b983828e936386e1fd7d8f
Akinyele Cafe-Febrissy <me@akinyele.ca>
2021-09-21 22:15:44 -0400

    type cover_art

--- lib/mangadex/cover_art.rb
+++ lib/mangadex/cover_art.rb
@@ -11,69 +11,79 @@ module Mangadex
       :updated_at,
       :version
 
-    class << self
-      def list(**args)
-        Mangadex::Internal::Request.get(
-          '/cover',
-          Mangadex::Internal::Definition.validate(args, {
-            limit: { accepts: Integer },
-            offset: { accepts: Integer },
-            manga: { accepts: [String] },
-            ids: { accepts: [String] },
-            uploaders: { accepts: [String] },
-            order: { accepts: Hash },
-            includes: { accepts: [String] },
-          })
-        )
-      end
-
-      def upload(file, volume=nil, manga_id:)
-        args = { file: file, volume: volume }
-        Mangadex::Internal::Request.post(
-          '/cover/%{manga_id}' % {manga_id: manga_id},
-          payload: Mangadex::Internal::Definition.validate(args, {
-            file: { accepts: String, required: true },
-            volume: { accepts: %r{^(0|[1-9]\\d*)((\\.\\d+){1,2})?[a-z]?$} } # todo: double check regexp here
-          })
-        )
-      end
-
-      def get(id, **args)
-        Mangadex::Internal::Request.get(
-          '/cover/%{id}' % {id: id},
-          Mangadex::Internal::Definition.validate(args, {
-            includes: { accepts: [String] },
-          })
-        )
-      end
-
-      def edit(id, **args)
-        Mangadex::Internal::Request.put(
-          '/cover/%{id}' % {id: id},
-          Mangadex::Internal::Definition.validate(args, {
-            volume: { accepts: String },
-            description: { accepts: String },
-            version: { accepts: Integer, required: true }
-          })
-        )
-      end
-
-      def delete(id)
-        Mangadex::Internal::Request.delete(
-          '/cover/%{id}' % {id: id},
-        )
-      end
+    sig { params(args: T::Api::Arguments).returns(Mangadex::Api::Response[CoverArt]) }
+    def self.list(**args)
+      Mangadex::Internal::Request.get(
+        '/cover',
+        Mangadex::Internal::Definition.validate(args, {
+          limit: { accepts: Integer },
+          offset: { accepts: Integer },
+          manga: { accepts: [String] },
+          ids: { accepts: [String] },
+          uploaders: { accepts: [String] },
+          order: { accepts: Hash },
+          includes: { accepts: [String] },
+        })
+      )
     end
 
+    sig do
+      params(
+        file: String,
+        volume: T.nilable(T.any(String, Integer)),
+        manga_id: String,
+      ).returns(Mangadex::Api::Response[CoverArt])
+    end
+    def self.upload(file, volume=nil, manga_id:)
+      args = { file: file, volume: volume }
+      Mangadex::Internal::Request.post(
+        '/cover/%{manga_id}' % {manga_id: manga_id},
+        payload: Mangadex::Internal::Definition.validate(args, {
+          file: { accepts: String, required: true },
+          volume: { accepts: %r{^(0|[1-9]\\d*)((\\.\\d+){1,2})?[a-z]?$} } # todo: double check regexp here
+        })
+      )
+    end
+
+    sig { params(id: String, args: T::Api::Arguments).returns(Mangadex::Api::Response[CoverArt]) }
+    def self.get(id, **args)
+      Mangadex::Internal::Request.get(
+        '/cover/%{id}' % {id: id},
+        Mangadex::Internal::Definition.validate(args, {
+          includes: { accepts: [String] },
+        })
+      )
+    end
+
+    sig { params(id: String, args: T::Api::Arguments).returns(Mangadex::Api::Response[CoverArt]) }
+    def self.edit(id, **args)
+      Mangadex::Internal::Request.put(
+        '/cover/%{id}' % {id: id},
+        Mangadex::Internal::Definition.validate(args, {
+          volume: { accepts: String },
+          description: { accepts: String },
+          version: { accepts: Integer, required: true }
+        })
+      )
+    end
+
+    sig { params(id: String).returns(Hash) }
+    def self.delete(id)
+      Mangadex::Internal::Request.delete(
+        '/cover/%{id}' % {id: id},
+      )
+    end
+
+    sig { params(size: T::Api::Text).returns(T.nilable(String)) }
     def image_url(size: :small)
       return unless manga.present?
-      
-      extension = case size
+
+      extension = case size.to_sym
       when :original
         ''
       when :medium
         '.512.jpg'
-      else # small by default
+      else # :small by default
         '.256.jpg'
       end
 
