f89112d0b058acd903203117fcdcf8292a8af12f
Akinyele Cafe-Febrissy <me@akinyele.ca>
2021-10-08 23:36:43 -0400

    Bump version 5.3.3.3: Fix nil error when fetching mangas

--- lib/mangadex/artist.rb
+++ lib/mangadex/artist.rb
@@ -2,10 +2,29 @@
 require_relative "author"
 
 module Mangadex
-  class Artist < Author
+  class Artist < MangadexObject
     # Indicates if this is an artist
     #
     # @return [Boolean] whether this is an artist or not.
+    has_attributes \
+      :name,
+      :image_url,
+      :biography,
+      :twitter,
+      :pixiv,
+      :melon_book,
+      :fan_box,
+      :booth,
+      :nico_video,
+      :skeb,
+      :fantia,
+      :tumblr,
+      :youtube,
+      :website,
+      :version,
+      :created_at,
+      :updated_at
+
     def artist?
       true
     end
--- lib/mangadex/version.rb
+++ lib/mangadex/version.rb
@@ -4,7 +4,7 @@ module Mangadex
     MAJOR = "5"
     MINOR = "3"
     TINY = "3"
-    PATCH = "2"
+    PATCH = "3"
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
     FULL = [MAJOR, MINOR, TINY, PATCH].compact.join('.')
