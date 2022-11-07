291094942e1300550746619a75ce2ddca0fec86e
Akinyele Cafe-Febrissy <me@akinyele.ca>
2022-11-07 01:00:01 -0500

    Bump to 5.7.5.2

--- lib/mangadex/version.rb
+++ lib/mangadex/version.rb
@@ -4,7 +4,7 @@ module Mangadex
     MAJOR = "5"
     MINOR = "7"
     TINY = "5"
-    PATCH = "1"
+    PATCH = "2"
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
     FULL = [MAJOR, MINOR, TINY, PATCH].compact.join('.')
