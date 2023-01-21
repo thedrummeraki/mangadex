cfb7278777582062599e174b863bdd9a1061fc1e
Akinyele Cafe-Febrissy <me@akinyele.ca>
2023-01-21 15:34:11 -0500

    Allow raw statistics

--- lib/mangadex/statistic.rb
+++ lib/mangadex/statistic.rb
@@ -30,31 +30,44 @@ module Mangadex
 
     class << self
       def from_data(data)
-        statistics = data[data.keys.first]
-        new(
-          rating: Mangadex::Statistic::Rating.from_data(statistics['rating'], direct: true),
-          comments: Mangadex::Statistic::Comments.from_data(statistics['comments'], direct: true),
-          follows: statistics['follows'],
-        )
+        results = if data.is_a?(Array)
+          data.map do |item|
+            from_data(item)
+          end
+        else
+          data.keys.map do |manga_id|
+            statistics = data[manga_id]
+            new(
+              rating: Mangadex::Statistic::Rating.from_data(statistics['rating'], direct: true),
+              comments: Mangadex::Statistic::Comments.from_data(statistics['comments'], direct: true),
+              follows: statistics['follows'],
+            )
+          end
+        end
+
+        return results.first if results.length == 1
+        Mangadex::Api::Response::Collection.new(results)
       end
     end
 
-    sig { params(uuid: String).returns(T::Api::GenericResponse) }
-    def self.get(uuid)
+    sig { params(uuid: String, raw: T::Boolean).returns(T::Api::GenericResponse) }
+    def self.get(uuid, raw: false)
       Mangadex::Internal::Definition.must(uuid)
 
       Mangadex::Internal::Request.get(
         '/statistics/manga/%{uuid}' % {uuid: uuid},
+        raw: raw,
       )
     end
 
-    sig { params(args: T::Api::Arguments).returns(T::Api::GenericResponse) }
-    def self.list(**args)
+    sig { params(raw: T::Boolean, args: T::Api::Arguments).returns(T::Api::GenericResponse) }
+    def self.list(raw: false, **args)
       Mangadex::Internal::Request.get(
         '/statistics/manga',
         Mangadex::Internal::Definition.validate(args, {
           manga: { accepts: [String], converts: :to_a },
-        })
+        }),
+        raw: raw,
       )
     end
 
